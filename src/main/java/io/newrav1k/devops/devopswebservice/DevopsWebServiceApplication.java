02d66548a207e7a363d5ccb439b848c8f1062de6
newrav1k <kritsky.academi@gmail.com>
2026-09-17 15:52:25 +0300

    Initial commit

---
+++ src/main/java/io/newrav1k/devops/devopswebservice/DevopsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package io.newrav1k.devops.devopswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/newrav1k/devops/devopswebservice/DevopsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.newrav1k.devops.devopswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
